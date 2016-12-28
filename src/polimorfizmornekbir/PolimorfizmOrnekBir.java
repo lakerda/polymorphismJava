8a600a2854504e889399b8f7fb5d39d25e9107e4
lakerda <canerdonmez1011@gmail.com>
2016-12-29 01:45:49 +0300

    polimorphism sample

---
+++ src/polimorfizmornekbir/PolimorfizmOrnekBir.java
@@ -0,0 +1,29 @@
+package polimorfizmornekbir;
+class Asker{
+    public void selamVer(){
+        System.out.println("asker selam verdi");
+    }
+}
+class er extends Asker{
+    public void selamVer(){
+            System.out.println("er selam verdi");
+    }
+}
+class Yuzbasi extends Asker{
+    public void selamVer(){
+            System.out.println("Y.basi selam verdi");
+    }
+}
+public class PolimorfizmOrnekBir {
+    public static void hazirOl(Asker a){
+            a.selamVer(); // dikkatttttttttttttttttttttttttttttttt
+    }
+    public static void main(String[] args) {
+        Asker a = new Asker();
+        er e = new er();
+        Yuzbasi y = new Yuzbasi();       
+        hazirOl(a);
+        hazirOl(e);
+        hazirOl(y);
+    }    
+}
